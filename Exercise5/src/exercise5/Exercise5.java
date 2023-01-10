f2158b9874c9166804484715fcf489cf74f65c3c
enieves <enieves@106-29.info2>
2023-01-10 10:42:35 +0100

    t

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,14 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Eloy");
+		System.out.println("Sevilla");
+		System.out.println("phonenumber");
+		
+		System.out.println("phonenumber");
+	}
+
+}

fe6087b118b13e4d96d1a49f9017f5eff722f2b6
enieves <enieves@106-29.info2>
2023-01-10 10:43:05 +0100

    t

---
+++ Exercise7/src/exercise7/Exercise7.java
@@ -0,0 +1,14 @@
+package exercise7;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" EEEEE       LL           OO        YY    YY  ");
+		System.out.println(" EE          LL         OO  OO       YY  YY   ");
+		System.out.println(" EEEEE       LL        OO    OO        YY     ");
+		System.out.println(" EE          LL         OO  OO         YY     ");
+		System.out.println(" EEEEE       LLLLL        OO           YY     ");
+	}
+
+}

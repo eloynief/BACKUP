15dd8d75d0982ac302c3f5642bd88a152d8ddfe4
enieves <enieves@106-29.info2>
2023-01-10 10:42:52 +0100

    t

---
+++ Exercise6/src/exercise6/Exercise6.java
@@ -0,0 +1,13 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Eloy");
+		System.out.println("Sevilla");
+		System.out.println("");
+		System.out.println("phonenumber 888888888888888888888888888888");
+	}
+
+}
